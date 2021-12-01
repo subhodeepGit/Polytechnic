df21c7a52c1f79f2d80c0550c5a09d7675d1d9ed
Subhajit-SOUL <subhajit.saha@soulunileaders.com>
2021-12-01 13:48:54 +0530

    Workspace modified

---
+++ polytechnic/polytechnic/doctype/test_polytechnic/test_polytechnic.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, SUSTAINABLE OUTREACH AND UNIVERSAL LEADERSHIP LIMITED and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class TestPolytechnic(Document):
+	pass
---
+++ polytechnic/polytechnic/doctype/test_polytechnic/test_test_polytechnic.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, SUSTAINABLE OUTREACH AND UNIVERSAL LEADERSHIP LIMITED and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestTestPolytechnic(unittest.TestCase):
+	pass
