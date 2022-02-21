81f9c5e9cca8dcf47c819eb897790efa82cf009e
Subhajit-SOUL <subhajit.saha@soulunileaders.com>
2022-02-21 13:11:03 +0530

    fix: program Enrollment

--- polytechnic/polytechnic/validations/program_enrollment.py
+++ polytechnic/polytechnic/validations/program_enrollment.py
@@ -1,28 +1,27 @@
 import frappe
 
 @frappe.whitelist()
+
 # def get_students(doctype, txt, searchfield, start, page_len, filters):
 #     return frappe.db.sql("""
 #                                 Select 
 #                                         distinct(st.name) as student, st.title as student_name,st.kiit_polytechnic_roll_no,st.vidyarthi_portal_id,st.sams_portal_id 
 #                                 from `tabCurrent Educational Details` ced 
-#                                 left join `tabStudent` st on st.name=ced.parent 
-
-#                                 where enabled=1 and (st.`{0}` LIKE %(txt)s or st.title  LIKE %(txt)s or 
-#                                 st.kiit_polytechnic_roll_no LIKE %(txt)s or st.vidyarthi_portal_id LIKE %(txt)s or
-#                                 st.sams_portal_id LIKE %(txt)s ) and ced.programs='{1}'
-#                                     """.format(searchfield,filters.get("programs")),dict(txt="%{}%".format(txt)))    
+#                                 left join `tabStudent` st on st.name=ced.parent
+#                                 where enabled=1 and (st.name LIKE %(txt)s or st.title LIKE %(txt)s) 
+#                                 or 
+#                                 st.kiit_polytechnic_roll_no LIKE %(txt)s 
+#                                 or 
+#                                 st.vidyarthi_portal_id LIKE %(txt)s 
+#                                 or
+#                                 st.sams_portal_id LIKE %(txt)s ) and ced.programs='{0}'""".format(filters.get("programs")),dict(txt="%{}%".format(txt)))
 
 def get_students(doctype, txt, searchfield, start, page_len, filters):
     return frappe.db.sql("""
                                 Select 
                                         distinct(st.name) as student, st.title as student_name,st.kiit_polytechnic_roll_no,st.vidyarthi_portal_id,st.sams_portal_id 
                                 from `tabCurrent Educational Details` ced 
-                                left join `tabStudent` st on st.name=ced.parent
-                                where enabled=1 and (st.name LIKE %(txt)s or st.title LIKE %(txt)s) 
-                                or 
-                                st.kiit_polytechnic_roll_no LIKE %(txt)s 
-                                or 
-                                st.vidyarthi_portal_id LIKE %(txt)s 
-                                or
-                                st.sams_portal_id LIKE %(txt)s ) and ced.programs='{0}'""".format(filters.get("programs")),dict(txt="%{}%".format(txt)))
+                                left join `tabStudent` st on st.name=ced.parent 
+                                where enabled=1 and (st.`{0}` LIKE %(txt)s or st.title  LIKE %(txt)s or 
+                                st.kiit_polytechnic_roll_no LIKE %(txt)s or st.vidyarthi_portal_id LIKE %(txt)s or
+                                st.sams_portal_id LIKE %(txt)s ) and ced.programs='{1}'""".format(searchfield,filters.get("programs")),dict(txt="%{}%".format(txt)))  
