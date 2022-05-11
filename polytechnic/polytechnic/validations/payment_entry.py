c32602569644de7cb33b71ca322b0acafed8158e
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-05-11 18:08:55 +0530

    fix:payment entry roll no search

--- polytechnic/polytechnic/validations/payment_entry.py
+++ polytechnic/polytechnic/validations/payment_entry.py
@@ -2,8 +2,7 @@ import frappe
 
 def validate(self, method):
     if self.party_type=="Student":
-        student_info=frappe.db.get_list("Student",filters={"name":self.party},fields=["sams_portal_id","kiit_polytechnic_roll_no","vidyarthi_portal_id"])
-        self.kiit_polytechnic_roll_no=student_info[0]['kiit_polytechnic_roll_no']
+        student_info=frappe.db.get_list("Student",filters={"name":self.party},fields=["sams_portal_id","vidyarthi_portal_id"])
         self.sams_portal_id=student_info[0]["sams_portal_id"]
         self.vidyarthi_portal_id=student_info[0]["vidyarthi_portal_id"]
         student_data_info=frappe.db.get_list("Current Educational Details",filters={"parent":self.party},fields=["academic_year"])
