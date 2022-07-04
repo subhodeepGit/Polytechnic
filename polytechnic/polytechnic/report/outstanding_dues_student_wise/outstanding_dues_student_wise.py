e1fe2ae11b7045efe9e176c18bea588118a5afb5
abhishekadhikari88 <aadhikari@kiit.ac.in>
2022-07-04 17:14:26 +0530

    outsatnding report

---
+++ polytechnic/polytechnic/report/outstanding_dues_student_wise/outstanding_dues_student_wise.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, SUSTAINABLE OUTREACH AND UNIVERSAL LEADERSHIP LIMITED and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+def execute(filters=None):
+	columns, data = [], []
+	return columns, data
