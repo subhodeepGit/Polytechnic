438a428db8d6f33e9e9525778615d7ac88c21825
abhishekadhikari88 <aadhikari@kiit.ac.in>
2022-08-16 16:30:13 +0530

    PRN value fetch

--- polytechnic/polytechnic/validations/payment_entry.py
+++ polytechnic/polytechnic/validations/payment_entry.py
@@ -4,9 +4,10 @@ from kp_edtec.kp_edtec.doctype.user_permission import add_user_permission
 
 def validate(self, method):
     if self.party_type=="Student":
-        student_info=frappe.db.get_list("Student",filters={"name":self.party},fields=["sams_portal_id","vidyarthi_portal_id"])
+        student_info=frappe.db.get_list("Student",filters={"name":self.party},fields=["sams_portal_id","vidyarthi_portal_id","permanant_registration_number"])
         self.sams_portal_id=student_info[0]["sams_portal_id"]
         self.vidyarthi_portal_id=student_info[0]["vidyarthi_portal_id"]
+        self.permanent_registration_number=student_info[0]["permanant_registration_number"]
         student_data_info=frappe.db.get_list("Current Educational Details",filters={"parent":self.party},fields=["academic_year"])
         self.academic_year=student_data_info[0]["academic_year"]
 
