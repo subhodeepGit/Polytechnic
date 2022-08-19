b1124d4f739aea110d5e7edb6910bab618560679
abhishekadhikari88 <aadhikari@kiit.ac.in>
2022-08-19 13:31:07 +0530

    print format word

---
+++ polytechnic/polytechnic/validations/student.py
@@ -0,0 +1,10 @@
+import frappe
+from frappe.model.document import Document
+
+def validate(self, method):
+    dob = self.date_of_birth
+    if dob:
+        from datetime import date,datetime 
+        mydate = datetime.strptime(dob,'%Y-%m-%d')
+        words_date=mydate.strftime("%A,%d %B, %Y")
+        self.dob_in_words = words_date
