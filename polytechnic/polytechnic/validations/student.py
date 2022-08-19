7610ed1a94cd55fe422456ef663253164a8baa14
Subhajit-SOUL <subhajit.saha@soulunileaders.com>
2022-08-19 15:34:01 +0530

    fix:dob date

--- polytechnic/polytechnic/validations/student.py
+++ polytechnic/polytechnic/validations/student.py
@@ -5,6 +5,6 @@ def validate(self, method):
     dob = self.date_of_birth
     if dob:
         from datetime import date,datetime 
-        mydate = datetime.strptime(dob,'%Y-%m-%d')
+        mydate = datetime.strptime(str(dob),'%Y-%m-%d')
         words_date=mydate.strftime("%A,%d %B, %Y")
         self.dob_in_words = words_date
