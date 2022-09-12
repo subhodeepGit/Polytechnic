049a845b9844c497f129efeb4e1708d532bddcf9
abhishekadhikari88 <aadhikari@kiit.ac.in>
2022-09-12 18:38:30 +0530

    update in payment entry report

---
+++ polytechnic/polytechnic/report/collection_report_base_on_payment_entry/collection_report_base_on_payment_entry.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2022, SUSTAINABLE OUTREACH AND UNIVERSAL LEADERSHIP LIMITED and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Collection Report Base On Payment Entry"] = {
+	"filters": [
+		{
+			"fieldname": "mode_of_payment",
+			"label": __("Mode of Payment"),
+			"fieldtype": "Link",
+			"options": "Mode of Payment",
+			"width": 150,
+			"reqd": 1,
+		},
+		{
+			"fieldname": "start_date",
+			"label": __("Start Date"),
+			"fieldtype": "Date",
+			"width": 80,
+			"reqd": 1,
+			"default": dateutil.year_start()
+		},
+		{
+			"fieldname": "end_date",
+			"label": __("End Date"),
+			"fieldtype": "Date",
+			"width": 80,
+			"reqd": 1,
+			"default": dateutil.year_end()
+		},
+	]
+};
