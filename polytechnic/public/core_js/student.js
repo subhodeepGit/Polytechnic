6400c0c44a30e98094fd916fda82808c8a308fcb
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-09-13 18:00:05 +0530

    hide button for transport user

--- polytechnic/public/core_js/student.js
+++ polytechnic/public/core_js/student.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Student', {
     onload:function(frm) {
-		if(frappe.user.has_role(["Fee Waiver"]) && !frappe.user.has_role(["Administrator"])){
+		if(frappe.user.has_role(["Fee Waiver","Transport User"]) && !frappe.user.has_role(["Administrator"])){
   			frm.remove_custom_button('Accounting Ledger');
         }
 	}
